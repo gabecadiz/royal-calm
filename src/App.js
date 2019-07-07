d763e3fc3c4c396007745a384646b8f0ee9f7ab2
Computer <gabecadiz@hotmail.com>
2019-07-07 17:08:50 -0400

    updated menu-items font, content styling and withRouter HOC for future routing purposes, product pages will need to be made before links can be correctly placed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 
+const HatPage = () => {
+  return (
+    <div>
+      HAT PAGE
+   </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatPage} />
       </Switch>
     </div>
   );
