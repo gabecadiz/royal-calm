48b1c5b8d566f255341bf5a19a9a67a1fe996eff
Computer <gabecadiz@hotmail.com>
2019-07-07 18:05:16 -0400

    created shop page, collection preview component and collection item component, item component needs add-to-cart button in future

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import './App.css';
-
-const HatPage = () => {
-  return (
-    <div>
-      HAT PAGE
-   </div>
-  )
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
