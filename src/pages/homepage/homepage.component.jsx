e35044807ce17527e7cfed5fad02690024fdf55d
Computer <gabecadiz@hotmail.com>
2019-07-07 14:07:03 -0400

    restructured homepage to be broken into more components, styled menu item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MenuContainer from '../../components/menu-container/menu-container.component'
+
+import './homepage.styles.scss'
+const HomePage = () => (
+  <div className='homepage'>
+    <MenuContainer />
+  </div>
+)
+
+export default HomePage
