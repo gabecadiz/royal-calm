f02d7c4cd5ae632abf07687ad66b962ca254ea13
Computer <gabecadiz@hotmail.com>
2019-07-07 23:31:59 -0400

    created signin page with signin, forminput and custombutton component with applicable styles, signin logic not implemented yet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
+import Header from './components/header/header.component'
 import App from './App';
 
 
 ReactDOM.render(
   <BrowserRouter>
+    <Header />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
