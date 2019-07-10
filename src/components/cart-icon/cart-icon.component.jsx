2ba11c0523e555a715cc4315a63cc605bcc86c1d
Computer <gabecadiz@hotmail.com>
2019-07-10 00:06:22 -0400

    added reselect package, created memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,19 +6,24 @@ import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { toggleCart } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 
 
-const CartIcon = ({ toggleCart }) => {
+const CartIcon = ({ toggleCart, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCart}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'> 0 </span>
+      <span className='item-count'> {itemCount} </span>
     </div>
   )
 }
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCart: () => dispatch(toggleCart())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
--- src/components/shopping-cart/shopping-cart.component.jsx
+++ src/components/shopping-cart/shopping-cart.component.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
+import { connect } from 'react-redux'
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import { selectCartItems, selectCartItemsPriceTotal } from '../../redux/cart/cart.selectors'
+
 
 import './shopping-cart.styles.scss';
 
-const ShoppingCart = () => {
+const ShoppingCart = ({ cartItems, totalPrice }) => {
   return (
     <div className="shopping-cart">
       <div className="cart-items">
-
+        {
+          cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+        }
+      </div>
+      <div className='total-container'>
+        <span className="price-info">
+          {totalPrice ? `Your cart total is $${totalPrice}` : "Your Cart Is Empty"}
+        </span>
       </div>
       <CustomButton>CHECKOUT</CustomButton>
     </div>
   )
 }
 
-export default ShoppingCart
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+  totalPrice: selectCartItemsPriceTotal(state)
+});
+
+export default connect(mapStateToProps)(ShoppingCart)
