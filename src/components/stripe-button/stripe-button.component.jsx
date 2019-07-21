3cc397d7386abd7e1a901338efe02b1f750a480d
Computer <gabecadiz@hotmail.com>
2019-07-21 15:53:06 -0400

    added clear cart and redirect to homepage logic to stripe button upon successful checkout

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { clearCart } from '../../redux/cart/cart.actions';
+
+import { withRouter } from "react-router";
+
+
 import StripeCheckout from 'react-stripe-checkout';
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, clearCart, history }) => {
   //stripe wants price in cents and USD
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_Cs8H9ipLhFjoatE9YWuyZc2O00AXCxO9Yi';
@@ -11,6 +17,8 @@ const StripeCheckoutButton = ({ price }) => {
   const onToken = token => {
     console.log(token);
     alert('Payment Successful');
+    clearCart();
+    history.push('/')
   }
 
   return (
@@ -29,4 +37,8 @@ const StripeCheckoutButton = ({ price }) => {
   )
 }
 
-export default StripeCheckoutButton;
+const mapDispatchToProps = dispatch => ({
+  clearCart: () => dispatch(clearCart())
+})
+
+export default withRouter(connect(null, mapDispatchToProps)(StripeCheckoutButton));
